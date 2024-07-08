ec38b34df962b5a591f5b546f4e8b5321d5d12a1
wiameouk <wiamoukaili7@gmail.com>
2024-07-08 12:59:21 +0200

    UpdateTableMultiplication

--- TableDemultiplication/TableMultiple.java
+++ TableDemultiplication/TableMultiple.java
@@ -1,51 +1,17 @@
+import java.util.Scanner;
+
 class TableMultiple{
     public static void main(String[]args)
     {
-        int tabmultiple=0;
-        int number=2;
-        int []tab={0,1,2,3,4,5,6,7,8,9};
-        for(int i=0;i<tab.length;i++)
+        int tabmultiple=1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Saisir un nombre ");
+        int number = scanner.nextInt(); 
+        for(int i=0;i<=6;i++)
         {
             tabmultiple=number*i;
             System.out.println(tabmultiple);
-
         }
 
     }
 }
-/*
- class TableMultiple{
-    public static void main(String[]args)
-    {
-        Scanner scanner = new Scanner( System.in);
-        System.out.print( " saisir un nomber : ");
-        int number=scanner.nextIn();
-        int ntabmultiple=0;
-        int []tab=
-        for(int i=0;i<=9;i++)
-        {
-            tabmultiple=number*i;
-            System.out.println(tabmultiple);
-
-        }
-
-    }
-}
- */
-/*
- class TableMultiple{
-
-   public static void main(String[]args)
-    {
-        int tabmultiple=0;
-        int number=2;
-        for(int i=0;i<=9;i++)
-        {
-            tabmultiple=number*i;
-            System.out.println( +number*i  +tabmultiple);
-
-        }
-
-    }
-}
- */
