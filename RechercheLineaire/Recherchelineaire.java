17c3dfda2e0798a8329ca517ce21d794fae76743
wiameouk <wiamoukaili7@gmail.com>
2024-07-08 13:38:43 +0200

    UpdateRecherchelineaire

--- RechercheLineaire/Recherchelineaire.java
+++ RechercheLineaire/Recherchelineaire.java
@@ -1,18 +1,29 @@
-class Recherchelineaire{
-    public static void main(String[] args)
-    {
-        int []tab={12,13,14,15,16,17,18};
-        for(int i=0;i<tab.length;i++)
-        {
-            if(tab[i] == 13)
-            {
-                System.out.println("existe l'element recherche"+tab[i]);
+import java.util.Scanner;
+
+class Recherchelineaire {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("donne un nombre recherche");
+        int nombre = scanner.nextInt();
+        System.out.print("donne la taille du tableau");
+        int taille = scanner.nextInt();
+        int[] tab = new int[taille];
+        System.out.println("Remplir le tableau");
+        for (int i = 0; i < taille; i++) {
+            tab[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < tab.length; i++) {
+            if (tab[i] == nombre) {
+                System.out.println("Rechcerche lineare du nombre est:"+nombre);
+                System.out.println("l'indexe "+i);
                 break;
             }
-            else{
-                System.out.println("n'existe pas l'element");
-            }
-
-        }
+        }    
     }
-}
+}    
+
+       
+            
+
+    
+
