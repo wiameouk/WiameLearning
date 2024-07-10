e8854619c58651f47b4313e30e543a7049909041
wiameouk <wiamoukaili7@gmail.com>
2024-07-10 21:07:13 +0200

    Succes RechercheBinaire

--- RechercheBinaire/Recherchebinaire.java
+++ RechercheBinaire/Recherchebinaire.java
@@ -1,12 +1,26 @@
-class Rechercherbinaire{
-    public static void main(String[] args)
-    {
-       int [] tab={2,0,1,4,1};
-        for(int i=0;i<tab.length;i++)
-        {
-            if(tab[i]==0 || tab[i]==1){
-                System.out.println("le nombre est binaire="+tab[i]);
+public class Recherchebinaire {
+    public static void main(String[] args) {
+        int[] tab = {2, 3, 4, 10, 40, 50, 80};
+        int elementbinaire = 10;
+        int debut = 0;
+        int fin = tab.length - 1;
+
+        while (debut <= fin) {
+            int milieu = (debut + fin) / 2;
+
+            if (tab[milieu] == elementbinaire) {
+                System.out.println("element Binaire est:"+elementbinaire );
+                System.out.println("indice est :"+milieu);
+                break; 
+            }
+
+            if (tab[milieu] < elementbinaire) {
+                debut = milieu + 1;
+            } else {
+                fin = milieu - 1;
             }
         }
     }
 }
+
+
