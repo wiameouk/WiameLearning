7dcc56b507f647181317bca97746d88367f2d0c7
wiameouk <wiamoukaili7@gmail.com>
2024-07-11 22:34:03 +0200

    Succes CalcluleMasse

--- CalculeMassecorporelle/Calculemassecorporelle.java
+++ CalculeMassecorporelle/Calculemassecorporelle.java
@@ -3,33 +3,25 @@ import java.util.Scanner;
 class  Calculemassecorporelle{
     public static void main(String[] args )
     {
-        System.out.println("Donne la taille");
-        Scanner scanner = new Scanner(System.in);  
-        float taille= scanner.nextFloat();
-        System.out.println("Donne la taille");
-        float poids= scanner.nextFloat();
-        float masseCorporelle= poids*(taille/taille);
-        if(masseCorporelle<18.5)
-        {
-            System.out.printf("Maigreur"+masseCorporelle);
-        }
-        else if(masseCorporelle > 18.5 && masseCorporelle<25)
-        {
-            System.out.println("normale"+masseCorporelle);
-        }
-        else if(masseCorporelle>=25 && masseCorporelle<30)
-        {
-            System.out.println("Surpoids"+masseCorporelle);
-        }
-        else if(masseCorporelle >=30 && masseCorporelle<35)
-        {
-            System.out.println("obésité"+masseCorporelle);
-        }
-        else 
-        {
-            System.out.println("Obésité sévere "+ masseCorporelle);
-        }
-        scanner.close();
+        Scanner scanner = new Scanner(System.in);
+          System.out.println("Donne votre taille  : ");
+          float taille = scanner.nextFloat();
+          System.out.println("Donne votre poids  : ");
+          float poids = scanner.nextFloat();
+          float masseCorporelle = poids / (taille * taille);
+            if (masseCorporelle < 18.5) {
+              System.out.printf("Maigreur : "+ masseCorporelle);
+          } else if (masseCorporelle >= 18.5 && masseCorporelle < 25) {
+              System.out.printf("Normale : "+ masseCorporelle);
+          } else if (masseCorporelle >= 25 && masseCorporelle < 30) {
+              System.out.printf("Surpoids :"+masseCorporelle);
+          } else if (masseCorporelle >= 30 && masseCorporelle < 35) {
+              System.out.printf("Obésité : "+masseCorporelle);
+          } else {
+              System.out.printf("Obésité sévère : " +masseCorporelle);
+          }
+  
+          scanner.close();
 
     }
 }
