65789d2380c4d2a0b2d83cd2424fefb5182e07a8
wiameouk <wiamoukaili7@gmail.com>
2024-07-13 21:43:28 +0200

    Succes palindrome

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,32 +1,23 @@
 import java.util.Scanner;
-class Palindrome{
+
+class Palindrome {
     public static void main(String[] args) {
         System.out.print("Donne le mot: ");
         Scanner scanner = new Scanner(System.in);  
         String chaine = scanner.nextLine();
-        int  taille = chaine.length();
-        char[] charArray = chaine.toCharArray();
-        boolean verification=false;
-        for(int i = 0; i < taille/2; i++)
-        {
-            if(charArray[i]==charArray[taille-1-i]){
-                verification=true;
-                break;  
-            }
-        }
-        if(verification==true)
-        {
+        System.out.println("la chaine : " + chaine);
+
+        String chaineInverse = new StringBuilder(chaine).reverse().toString();
+        System.out.println("chaine inversé: " + chaineInverse);
+
+        if (chaine.equals(chaineInverse)) {
             System.out.println("Le mot est un palindrome.");
+        } else {
+            System.out.println("Le mot n'est pas un palindrome.");
         }
-        else{
-            System.out.println("Le mot n'est pas  un palindrome.");
 
-        }
-           
-        
     }
-  
-
 }
+
 
 
