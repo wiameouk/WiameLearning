5315e9bbc19379fb2a6d164066bb2f01a27b3795
wiameouk <wiamoukaili7@gmail.com>
2024-10-10 23:12:43 +0200

    frq

---
+++ Analysetexte/Main.java
@@ -0,0 +1,6 @@
+class Main {
+    public static void main(String[] args) {
+        String paraghraphe = "Bonjour je suis etudiante emsi , emsi je suis wiam oukaili , bonjour ";
+        System.out.println(Analysetexte.frequence(paraghraphe));
+    }
+}
