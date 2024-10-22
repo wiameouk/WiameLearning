cdb300b0110a3f259f17a16a29b0c60cf0a530e3
wiameouk <wiamoukaili7@gmail.com>
2024-10-22 20:54:11 +0200

    update

--- CalculTemps/MesureTemps.java
+++ CalculTemps/MesureTemps.java
@@ -3,19 +3,17 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-public class MesureTemps {
- 
+public class MesureTemps { 
     public static void main(String[] args) {
         int taille = 10;
         List<Integer> randomNumbers = new ArrayList<>();
         for (int i = 0; i < taille; i++) {
             randomNumbers.add(new Random().nextInt(100));
         }
-
-        int start = (int) System.currentTimeMillis();
+        long start = System.currentTimeMillis();
         Collections.sort(randomNumbers);
-        int end = (int) System.currentTimeMillis();
-        int temps_mesure = end - start;
+        long end = System.currentTimeMillis();
+        long temps_mesure = end - start;
         System.out.println("temps mesure entre les deux est : "+temps_mesure);
     }
     
